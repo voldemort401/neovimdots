5b79eb33d5c5e41ff7573270d4d022f693e0def6
voldemort401 <181987124+voldemort401@users.noreply.github.com>
2025-06-01 09:43:06 +0545

    upd: config

--- lua/vld/config/nvim_conf/init.lua
+++ lua/vld/config/nvim_conf/init.lua
@@ -1,9 +1,3 @@
 require ("vld.config.nvim_conf.opts")
 require ("vld.config.nvim_conf.keymaps")
-
-
---lsps 
-vim.lsp.enable('clangd')
-vim.lsp.enable('pyright')
-vim.lsp.enable('cssls')
-vim.lsp.enable('rust_analyzer')
+require ("vld.config.lspconf")
